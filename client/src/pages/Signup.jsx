ded307b8984566fcbe5da064dc2bfc28499e24ac
Oladoye-Ajiboye4 <oladoyeajiboye@gmail.com>
2026-02-25 13:01:16 +0100

    Refactor Homepage, Signin, and Signup components for improved layout and styling; fix tailwindcss plugin reference in vite.config.js

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <>
+      <main className='flex flex-col justify-center items-center w-full h-screen gap-10'>
+
+        <h1 className='text-4xl font-bold'>Sign Up</h1>
+        <div className='flex gap-4'>
+          <Link className='bg-amber-700/50 rounded-2xl p-5 text-white font-bold text-lg hover:bg-amber-700' to='/signin'><button>Sign In</button></Link>
+          <Link className='bg-blue-700/50 rounded-2xl p-5 text-white font-bold text-lg hover:bg-blue-700' to='/'><button>Go Home</button></Link>
+
+        </div>
+      </main>
+    </>
   )
 }
 
