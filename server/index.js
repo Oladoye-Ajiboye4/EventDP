b9dc98a9724e88a07fcb8ee15f5156cc4ef91395
Oladoye-Ajiboye4 <oladoyeajiboye@gmail.com>
2026-04-04 23:25:36 +0100

    feat: configure CORS to allow requests from the specified origin

--- server/index.js
+++ server/index.js
@@ -32,7 +32,12 @@ const cors = require('cors')
 require('./config/cloudinary')
 startCloudinaryOrphanCleanupJob()
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://event-dp.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true 
+}));
+
 app.use(express.json({ limit: '2mb' }))
 app.use(express.urlencoded({ extended: true, limit: '2mb' }))
 
